12dcb116136afe6b68d6d47f6ea99c37a65b7a3b
Paola <paolareategui@hotmail.com>
2024-09-22 12:11:40 +1000

    Replace google_routes with google_polylines_points package to deal with version incompatibility
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- lib/widgets/google_map_view.dart
+++ lib/widgets/google_map_view.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:google_maps_routes/google_maps_routes.dart';
 import 'package:the_carbon_conscious_traveller/constants.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 
 class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
@@ -27,34 +27,25 @@ class GoogleMapViewState extends State<GoogleMapView> {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
-  /// LatLng is included in google_maps_flutter
-  List<LatLng> points = [
-    LatLng(_kGooglePlex.target.latitude, _kGooglePlex.target.longitude),
-    LatLng(_kDestinantion.target.latitude, _kDestinantion.target.longitude),
-  ];
-
-  MapsRoutes route = MapsRoutes();
+  PolylinePoints polylinePoints = PolylinePoints();
+  Map<PolylineId, Polyline> polylines = {};
+  List<LatLng> polylineCoordinates = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: _onMapCreated,
-        markers: markers,
-        polylines: route.routes,
-      ),
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: _onMapCreated,
+          markers: markers,
+          polylines: Set<Polyline>.of(polylines.values)),
     );
   }
 
   void _onMapCreated(GoogleMapController controller) async {
     _controller.complete(controller);
-    // Add route to map
-    await route.drawRoute(points, "test route",
-        const Color.fromRGBO(130, 78, 210, 1.0), Constants.googleApiKey,
-        travelMode: TravelModes.driving);
-    //Display markers
+    _getPolyline();
     setState(() {
       markers.add(
         Marker(
@@ -70,5 +61,35 @@ class GoogleMapViewState extends State<GoogleMapView> {
         infoWindow: InfoWindow(title: 'Mountain View'),
       ));
     });
+  }
+
+  _drawPolyline() {
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+        polylineId: id,
+        color: Colors.red,
+        points: polylineCoordinates,
+        width: 5);
+    polylines[id] = polyline;
+    setState(() {/*the polyline is drawn*/});
+  }
+
+  _getPolyline() async {
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      googleApiKey: Constants.googleApiKey,
+      request: PolylineRequest(
+        origin: PointLatLng(
+            _kGooglePlex.target.latitude, _kGooglePlex.target.longitude),
+        destination: PointLatLng(
+            _kDestinantion.target.latitude, _kDestinantion.target.longitude),
+        mode: TravelMode.driving,
+      ),
+    );
+    if (result.points.isNotEmpty) {
+      for (PointLatLng point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+    _drawPolyline();
   }
 }
