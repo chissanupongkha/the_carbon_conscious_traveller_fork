ef81ebf68ca756ebba73d0ed00d0e6a651f76385
Paola <paolareategui@hotmail.com>
2024-10-04 10:12:36 +1000

    Display alternate routes
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- lib/models/polyline_model.dart
+++ lib/models/polyline_model.dart
@@ -6,14 +6,18 @@ import 'package:the_carbon_conscious_traveller/constants.dart';
 class PolylineModel extends ChangeNotifier {
   final PolylinePoints _polylinePoints = PolylinePoints();
   final Map<PolylineId, Polyline> _polylines = {};
-  final List<LatLng> _polylineCoordinates = [];
+  //final List<LatLng> _polylineCoordinates = [];
+  final List<List<LatLng>> _routeCoordinates = [];
   TravelMode _transportMode = TravelMode.driving;
   String _mode = 'driving';
+  int _activeRouteIndex = 0;
 
   PolylinePoints get polylinePoints => _polylinePoints;
   Map<PolylineId, Polyline> get polylines => _polylines;
-  List<LatLng> get polylineCoordinates => _polylineCoordinates;
+  //List<LatLng> get polylineCoordinates => _polylineCoordinates;
   String get mode => _mode;
+  List<List<LatLng>> get routeCoordinates => _routeCoordinates;
+  int get activeRouteIndex => _activeRouteIndex;
 
   static const Map<String, TravelMode> _modeMap = {
     'driving': TravelMode.driving,
@@ -32,40 +36,77 @@ class PolylineModel extends ChangeNotifier {
 
   void resetPolyline() {
     print("Resetting polyline");
-    _polylineCoordinates.clear();
+    //_polylineCoordinates.clear();
     _polylines.clear();
+    _routeCoordinates.clear();
+    _activeRouteIndex = 0;
     notifyListeners();
   }
 
-  void drawPolyline() {
-    print("Drawing polyline");
-    PolylineId id = const PolylineId("poly");
-    Polyline polyline = Polyline(
-        polylineId: id,
-        color: const Color.fromARGB(255, 45, 61, 245),
-        points: polylineCoordinates,
-        width: 4);
-    polylines[id] = polyline;
-    notifyListeners();
-  }
+  // void drawPolyline() {
+  //   print("Drawing polyline");
+  //   PolylineId id = const PolylineId("poly");
+  //   Polyline polyline = Polyline(
+  //       polylineId: id,
+  //       color: const Color.fromARGB(255, 45, 61, 245),
+  //       points: polylineCoordinates,
+  //       width: 4);
+  //   polylines[id] = polyline;
+  //   notifyListeners();
+  // }
 
   void getPolyline(List<LatLng> coordinates) async {
     print("Getting polyline...");
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+
+    List<PolylineResult> result = await polylinePoints.getRouteWithAlternatives(
       googleApiKey: Constants.googleApiKey,
       request: PolylineRequest(
         origin: PointLatLng(coordinates[0].latitude, coordinates[0].longitude),
         destination:
             PointLatLng(coordinates[1].latitude, coordinates[1].longitude),
         mode: _transportMode,
+        alternatives: true,
       ),
     );
-    if (result.points.isNotEmpty) {
-      for (PointLatLng point in result.points) {
-        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+    if (result.isNotEmpty) {
+      for (int i = 0; i < result.length; i++) {
+        List<LatLng> routeCoordinate = [];
+
+        for (var point in result[i].points) {
+          routeCoordinate.add(LatLng(point.latitude, point.longitude));
+        }
+
+        routeCoordinates.add(routeCoordinate);
       }
+      _updateActiveRoute(0);
     }
-    drawPolyline();
+    //drawPolyline();
     notifyListeners();
+  }
+
+  void _updateActiveRoute(int index) {
+    _activeRouteIndex = index;
+    polylines.clear();
+
+    for (int i = 0; i < routeCoordinates.length; i++) {
+      PolylineId id = PolylineId('poly$i');
+      Polyline polyline = Polyline(
+        polylineId: id,
+        color: i == activeRouteIndex ? Colors.blue : Colors.grey,
+        points: routeCoordinates[i],
+        width: i == activeRouteIndex ? 5 : 3,
+        zIndex: i == _activeRouteIndex ? 1 : 0, // Put active route on top
+        consumeTapEvents: true,
+        onTap: () => setActiveRoute(i),
+      );
+      polylines[id] = polyline;
+    }
+    notifyListeners();
+  }
+
+  void setActiveRoute(int index) {
+    if (index >= 0 && index < _routeCoordinates.length) {
+      _updateActiveRoute(index);
+    }
   }
 }
