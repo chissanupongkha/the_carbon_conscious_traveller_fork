ea306e4398703ae8e2e4f7455028c0cb726d8848
Paola <paolareategui@hotmail.com>
2024-10-13 18:08:57 +1100

    Create enums for car emission values
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- lib/models/calculation_values.dart
+++ lib/models/calculation_values.dart
@@ -1,27 +1,219 @@
-const List<String> carSizes = [
-  "Small car",
-  "Medium car",
-  "Large car",
-  "Mini",
-  "Supermini",
-  "Lower medium",
-  "Upper medium",
-  "Executive",
-  "Luxury",
-  "Sports",
-  "Dual purpose 4X4",
-  "MPV"
-];
+// const List<String> carSizes = [
+//   "Small car",
+//   "Medium car",
+//   "Large car",
+//   "Mini",
+//   "Supermini",
+//   "Lower medium",
+//   "Upper medium",
+//   "Executive",
+//   "Luxury",
+//   "Sports",
+//   "Dual purpose 4X4",
+//   "MPV"
+// ];
 
-const List<String> carFuelTypes = [
-  "Diesel",
-  "Petrol",
-  "Hybrid",
-  "CNG",
-  "LPG",
-  "Plug-in Hybrid Electric Vehicle",
-  "Battery Electric Vehicle"
-];
+// const List<String> carFuelTypes = [
+//   "Diesel",
+//   "Petrol",
+//   "Hybrid",
+//   "CNG",
+//   "LPG",
+//   "Plug-in Hybrid Electric Vehicle",
+//   "Battery Electric Vehicle"
+// ];
+
+// const List<List<double>> carValuesMatrix = [
+//   [
+//     0.139306448880537,
+//     0.140798534228188,
+//     0.101498857718121,
+//     0.0,
+//     0.0,
+//     0.0540229932885906,
+//     0.0482282859060403
+//   ],
+//   [
+//     0.167156448880537,
+//     0.178188534228188,
+//     0.109038436241611,
+//     0.156604197315436,
+//     0.176070597315436,
+//     0.0850073342281879,
+//     0.0526663489932886
+//   ],
+//   [
+//     0.208586448880537,
+//     0.272238534228188,
+//     0.1524358,
+//     0.238454197315436,
+//     0.269140597315436,
+//     0.101584382550336,
+//     0.05737
+//   ],
+//   [
+//     0.107746448880537,
+//     0.130298534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0443381932885906
+//   ],
+//   [
+//     0.132146448880537,
+//     0.141688534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0540229932885906,
+//     0.0490671785234899
+//   ],
+//   [
+//     0.143456448880537,
+//     0.164728534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0831183489932886,
+//     0.0525663489932886
+//   ],
+//   [
+//     0.160496448880537,
+//     0.192108534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0868662268456376,
+//     0.0547703154362416
+//   ],
+//   [
+//     0.173096448880537,
+//     0.212318534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0888617973154363,
+//     0.0500662563758389
+//   ],
+//   [
+//     0.211196448880537,
+//     0.318088534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.115139275167785,
+//     0.0583732120805369
+//   ],
+//   [
+//     0.169436448880537,
+//     0.237158534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0996696416107383,
+//     0.0834804456375839
+//   ],
+//   [
+//     0.201946448880537,
+//     0.204048534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.103275582550336,
+//     0.0610424751677852
+//   ],
+//   [
+//     0.176596448880537,
+//     0.184258534228188,
+//     0.0,
+//     0.0,
+//     0.0,
+//     0.0990220751677852,
+//     0.0793324751677852
+//   ]
+// ];
+
+enum CarSize {
+  smallCar,
+  mediumCar,
+  largeCar,
+  mini,
+  supermini,
+  lowerMedium,
+  upperMedium,
+  executive,
+  luxury,
+  sports,
+  dualPurpose4x4,
+  mpv,
+}
+
+extension CarSizeExtension on CarSize {
+  String get name {
+    switch (this) {
+      case CarSize.smallCar:
+        return "Small car";
+      case CarSize.mediumCar:
+        return "Medium car";
+      case CarSize.largeCar:
+        return "Large car";
+      case CarSize.mini:
+        return "Mini";
+      case CarSize.supermini:
+        return "Supermini";
+      case CarSize.lowerMedium:
+        return "Lower medium";
+      case CarSize.upperMedium:
+        return "Upper medium";
+      case CarSize.executive:
+        return "Executive";
+      case CarSize.luxury:
+        return "Luxury";
+      case CarSize.sports:
+        return "Sports";
+      case CarSize.dualPurpose4x4:
+        return "Dual purpose 4X4";
+      case CarSize.mpv:
+        return "MPV";
+      default:
+        return "";
+    }
+  }
+}
+
+enum CarFuelType {
+  diesel,
+  petrol,
+  hybrid,
+  cng,
+  lpg,
+  phev,
+  bev,
+}
+
+extension CarFuelTypeExtension on CarFuelType {
+  String get name {
+    switch (this) {
+      case CarFuelType.diesel:
+        return "Diesel";
+      case CarFuelType.petrol:
+        return "Petrol";
+      case CarFuelType.hybrid:
+        return "Hybrid";
+      case CarFuelType.cng:
+        return "CNG";
+      case CarFuelType.lpg:
+        return "LPG";
+      case CarFuelType.phev:
+        return "Plug-in Hybrid Electric Vehicle";
+      case CarFuelType.bev:
+        return "Battery Electric Vehicle";
+      default:
+        return "";
+    }
+  }
+}
 
 const List<List<double>> carValuesMatrix = [
   [
