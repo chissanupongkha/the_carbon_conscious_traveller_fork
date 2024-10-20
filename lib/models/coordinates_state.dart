68e417bf000845eb9c4aecef4035e65904488209
Paola <paolareategui@hotmail.com>
2024-10-20 22:36:04 +1100

    Retrieve and display transit data using google directions api package
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- lib/models/coordinates_state.dart
+++ lib/models/coordinates_state.dart
@@ -3,10 +3,26 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CoordinatesState with ChangeNotifier {
   final List<LatLng> _coordinates = [];
+  LatLng _originCoords = const LatLng(0, 0);
+  LatLng _destinationCoords = const LatLng(0, 0);
 
   List<LatLng> get coordinates => _coordinates;
+  LatLng get originCoords => _originCoords;
+  LatLng get destinationCoords => _destinationCoords;
 
   void saveCoordinates(LatLng coordinate) {
+    coordinates.add(coordinate);
+    notifyListeners();
+  }
+
+  void saveOriginCoords(LatLng coordinate) {
+    _originCoords = coordinate;
+    coordinates.add(coordinate);
+    notifyListeners();
+  }
+
+  void saveDestinationCoords(LatLng coordinate) {
+    _destinationCoords = coordinate;
     coordinates.add(coordinate);
     notifyListeners();
   }
--- lib/widgets/travel_mode_transit.dart
+++ lib/widgets/travel_mode_transit.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_directions_api/google_directions_api.dart';
+import 'package:provider/provider.dart';
+import 'package:the_carbon_conscious_traveller/models/coordinates_state.dart';
+import 'package:the_carbon_conscious_traveller/models/routes_model.dart';
 
 class Transit extends StatefulWidget {
   const Transit({super.key});
@@ -8,15 +12,108 @@ class Transit extends StatefulWidget {
 }
 
 class _TransitState extends State<Transit> {
+  final travelMode = TravelMode.transit;
+  RoutesModel? routesModel;
+  dynamic routes;
+  String? iconURL;
+
+  Future<List<DirectionsRoute>> fetchRouteInfo() async {
+    debugPrint("request sent");
+    final coordsState = Provider.of<CoordinatesState>(context, listen: false);
+    routesModel = RoutesModel(
+      origin: GeoCoord(coordsState.originCoords.latitude,
+          coordsState.originCoords.longitude),
+      destination: GeoCoord(coordsState.destinationCoords.latitude,
+          coordsState.destinationCoords.longitude),
+      travelMode: TravelMode.transit,
+    );
+    debugPrint("request is about to return");
+    routes = await routesModel?.getRouteInfo();
+    debugPrint("routes $routes");
+    if (routes == null) {
+      return [];
+    } else {
+      debugPrint("routes returned? $routes");
+      return routes;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      width: 600,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Place holder for transit data"),
-        ],
+    return SizedBox(
+      width: 500,
+      height: 500,
+      child: FutureBuilder<List<DirectionsRoute>>(
+        future: fetchRouteInfo(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            print("snapshot data: ${snapshot.data!.length}");
+            return snapshot.data == null
+                ? const Text('No data available')
+                : SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            int? stepsIdx =
+                                snapshot.data?[index].legs?.first.steps?.length;
+                            int i = 0;
+                            return Column(
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    for (i = 0; i < stepsIdx!; i++) ...[
+                                      if (snapshot
+                                              .data?[index]
+                                              .legs
+                                              ?.first
+                                              .steps?[i]
+                                              .transit
+                                              ?.line
+                                              ?.vehicle
+                                              ?.icon ==
+                                          null)
+                                        const Icon(Icons.directions_walk)
+                                      else
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Image.network(
+                                                "https:${snapshot.data?[index].legs?.first.steps?[i].transit?.line?.vehicle?.icon}"),
+                                            Text(
+                                                "${snapshot.data?[index].legs?.first.steps?[i].transit?.line?.shortName}"),
+                                          ],
+                                        ),
+                                    ],
+                                  ],
+                                ),
+                                Text(
+                                    "${snapshot.data?[index].legs?.first.departureTime?.text} - ${snapshot.data?[index].legs?.first.arrivalTime?.text}"),
+                                Text(
+                                    "${snapshot.data?[index].legs?.first.distance?.text}"),
+                                Text(
+                                    "${snapshot.data?[index].legs?.first.duration?.text}"),
+                              ],
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                        ),
+                      ],
+                    ),
+                  );
+          } else {
+            return const Text('No data available');
+          }
+        },
       ),
     );
   }
